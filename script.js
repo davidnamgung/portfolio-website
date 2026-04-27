1da2f9384effb5d77a1b973f148a5665f0b5324e
David Namgung <130765632+davidnamgung@users.noreply.github.com>
2026-04-27 12:38:48 -0400

    removed everything, going back to normal

--- script.js
+++ script.js
@@ -40,33 +40,3 @@ fadeElements.forEach(element => {
 
 
 
-// =========================================
-// CUSTOM GOOGLE TRANSLATE TOGGLE LOGIC
-// =========================================
-
-document.addEventListener("DOMContentLoaded", function() {
-    const langToggle = document.getElementById('lang-toggle');
-
-    langToggle.addEventListener('click', function(e) {
-        e.preventDefault(); // Prevents the page from jumping to the top
-        
-        // Find the hidden Google Translate dropdown
-        const selectElement = document.querySelector('.goog-te-combo');
-
-        if (selectElement) {
-            // If the button says 'FR', switch page to French and change button to 'EN'
-            if (langToggle.innerText === 'FR') {
-                selectElement.value = 'fr';
-                langToggle.innerText = 'EN';
-            } 
-            // If the button says 'EN', switch page back to English and change button to 'FR'
-            else {
-                selectElement.value = 'en';
-                langToggle.innerText = 'FR';
-            }
-            
-            // Tell the hidden Google widget that we made a selection
-            selectElement.dispatchEvent(new Event('change'));
-        }
-    });
-});
